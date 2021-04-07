ad61c9e5d70a934caf6dafa3366c98fd45110d84
ashwinisapreknoxpo <ashwini.sapre@knoxpo.com>
2021-04-07 10:52:39 +0530

    Initial Commit

---
+++ APIRequestProtocolApproch/APIRequestProtocolApprochApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIRequestProtocolApprochApp.swift
+//  APIRequestProtocolApproch
+//
+//  Created by Knoxpo MacBook Pro on 07/04/21.
+//
+
+import SwiftUI
+
+@main
+struct APIRequestProtocolApprochApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
